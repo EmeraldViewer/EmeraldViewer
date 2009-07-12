242c1163eb4e92704f258fd88c3b1a4c0db07577
chris.tuchs <none@none>
2009-07-12 01:03:31 +0000

    cmake changes to cause the libotr, libgcrypt, and libgpg-error to be compiled but not used on windows.  Should not even try to compile on linux or mac.  Also an ifdefed out call to the lib otr library initialization routine in llappviewer.cpp
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%40259

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -188,6 +188,20 @@
 //----------------------------------------------------------------------------
 // viewer.cpp - these are only used in viewer, should be easily moved.
 
+#if COMPILE_OTR
+// $PLOTR$ OTR library includes.  See http://www.cypherpunks.ca/otr/
+extern "C" {
+#if LL_WINDOWS
+#include <unistd.h>             // various defines needed for OTR on windows
+#endif
+#include <gcrypt.h>		// gcrypt dependancies
+//#include <userstate.h>	// OTR
+#include <proto.h>		// OTR
+//#include <privkey.h>	// OTR
+//#include <message.h>	// OTR
+}
+#endif // COMPILE_OTR
+
 #if LL_DARWIN
 extern void init_apple_menu(const char* product);
 #endif // LL_DARWIN
@@ -571,6 +585,10 @@ bool LLAppViewer::init()
 	//
 	// OK to write stuff to logs now, we've now crash reported if necessary
 	//
+#if USE_OTR
+	OTRL_INIT; // $PLOTR$ Init OTR library
+#endif // USE_OTR
+
     if (!initConfiguration())
 		return false;
 
