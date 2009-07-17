82cf859a100e23d553ac954deab6254a8c609b42
jcool410 <none@none>
2009-07-17 20:18:42 +0000

    fix shader faggotry
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%40357

--- indra/newview/llstartup.cpp
+++ indra/newview/llstartup.cpp
@@ -324,7 +324,8 @@ bool idle_startup()
 {
 	LLMemType mt1(LLMemType::MTYPE_STARTUP);
 	
-	const F32 PRECACHING_DELAY = gSavedSettings.getF32("PrecachingDelay")+0.01;
+	const F32 PRECACHING_DELAY = gSavedSettings.getF32("PrecachingDelay")+0.1;
+	if(PRECACHING_DELAY == 0.1)gSavedSettings.setF32("PrecachingDelay",0.1);
 	const F32 TIMEOUT_SECONDS = 5.f;
 	const S32 MAX_TIMEOUT_COUNT = 3;
 	static LLTimer timeout;
