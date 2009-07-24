e9b073675e269fc81c6f7b583f8f4bad3f744c68
chris.tuchs <none@none>
2009-07-24 08:08:06 +0000

    Fix spelling of "Authenticate budy" to be "Authenticate buddy."  Make resumed OTR conv.s show the trust state in the logged IM line (private vs. unverified).
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%40422

--- indra/newview/otr_wrapper.cpp
+++ indra/newview/otr_wrapper.cpp
@@ -323,7 +323,16 @@ static void otrwui_still_secure(
         return;
     }
     LLUUID session_id = *((LLUUID*)opdata);
-    otr_log_message_getstring(session_id, "otr_log_still_secure");
+    if (context && context->active_fingerprint &&
+        context->active_fingerprint->trust &&
+        *(context->active_fingerprint->trust))
+    {
+        otr_log_message_getstring_name(session_id, "otr_log_still_private");
+    }
+    else
+    {
+        otr_log_message_getstring_name(session_id, "otr_log_still_unverified");
+    }
     show_otr_status(session_id);
 }
 
