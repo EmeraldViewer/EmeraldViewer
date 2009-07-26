a1b7dcb7fa6169fe34a058c0bcfdf5ca32c6023f
chalice <none@none>
2009-07-26 23:23:25 +0000

    Groundsit-everywhere can now be TOGGLED. Oh em gee.
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%40444

--- indra/newview/llviewermenu.cpp
+++ indra/newview/llviewermenu.cpp
@@ -8339,15 +8339,40 @@ class LLEmeraldToggleSit: public view_listener_t
 
 		if(gSavedSettings.getBOOL("EmeraldAllowSitToggle"))
 		{
-			gAgent.setControlFlags(AGENT_CONTROL_SIT_ON_GROUND);
 			LLChat chat;
 			chat.mSourceType = CHAT_SOURCE_SYSTEM;
-			chat.mText = "Forcing Ground Sit";
+			if(!gAgent.getAvatarObject()->mIsSitting)
+			{
+				gAgent.setControlFlags(AGENT_CONTROL_SIT_ON_GROUND);
+				chat.mText = "Forcing Ground Sit";
+			}
+			else
+			{
+				gAgent.setControlFlags(!AGENT_CONTROL_SIT_ON_GROUND);
+				gAgent.setControlFlags(AGENT_CONTROL_STAND_UP);
+				chat.mText = "Standing up";
+			}
 			LLFloaterChat::addChat(chat);
 		}
 		return true;
 	}
 
+};
+
+class LLEmeraldCheckSit : public view_listener_t
+{
+    bool handleEvent(LLPointer<LLEvent> event, const LLSD& userdata)
+    {
+		if(gAgent.getAvatarObject()->mIsSitting)
+		{
+			gMenuHolder->findControl(userdata["control"].asString())->setValue(true);
+		}
+		else
+		{
+			gMenuHolder->findControl(userdata["control"].asString())->setValue(false);
+		}
+		return true;
+	}
 };
 class LLEmeraldToggleRadar: public view_listener_t
 {
