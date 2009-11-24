39b57fb78984dfeb12f1d33b2032d4ea370ca0a1
jcool410 <none@none>
2009-11-24 14:45:36 +0000

    licensing fix ugh, also bridge tweak, texlayer no more retries
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401047

--- indra/newview/jc_lslviewerbridge.cpp
+++ indra/newview/jc_lslviewerbridge.cpp
@@ -1,3 +1,34 @@
+/* Copyright (c) 2009
+ *
+ * Modular Systems All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *   3. Neither the name Modular Systems nor the names of its contributors
+ *      may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY MODULAR SYSTEMS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MODULAR SYSTEMS OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 
 #include "llviewerprecompiledheaders.h"
 
@@ -234,7 +265,7 @@ BOOL JCLSLBridge::tick()
 				}
 				//cmdline_printchat("initializing");//<< llendl;
 				LLUUID item_id = findInventoryByName(vBridgeName);
-				if(gInventory.isEverythingFetched() || item_id.notNull())
+				if(gInventory.isEverythingFetched())// || (item_id.notNull() && isworn(item_id)))
 				{
 					//cmdline_printchat("inv is fetched");//<< llendl;
 					
@@ -329,7 +360,7 @@ BOOL JCLSLBridge::tick()
 		case RECHAN:
 			{
 				{
-					if(l2c == 0)
+					//if(l2c == 0)
 					{
 						send_chat_from_viewer("-1|l2c", CHAT_TYPE_WHISPER, JCLSLBridge::bridge_channel(gAgent.getID()));
 						sBridgeStatus = ACTIVE;
@@ -343,6 +374,8 @@ BOOL JCLSLBridge::tick()
 				//LLVOAvatar* avatar = gAgent.getAvatarObject();
 				if(bridge.isNull() || !isworn(bridge))
 				{
+					l2c = 0;
+					l2c_inuse = false;
 					////cmdline_printchat("reattaching");
 					sBridgeStatus = UNINITIALIZED;
 				}
