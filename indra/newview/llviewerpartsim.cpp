9eec39003cf63abe425685547f79eaacce5eccdf
zwagoth <none@none>
2010-01-08 07:28:09 +0000

    griff
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401303

--- indra/newview/llviewerpartsim.cpp
+++ indra/newview/llviewerpartsim.cpp
@@ -290,7 +290,7 @@ void LLViewerPartGroup::updateParticles(const F32 lastdt)
 		// "Drift" the object based on the source object
 		if (part->mFlags & LLPartData::LL_PART_FOLLOW_SRC_MASK)
 		{
-			if(part->mPartSourcep->getType() == LL_PART_SOURCE_SCRIPT)
+			if(part->mPartSourcep->getType() == LLViewerPartSource::LL_PART_SOURCE_SCRIPT)
 			{
 				LLViewerPartSourceScript* partsys = static_cast<LLViewerPartSourceScript*>(part->mPartSourcep.get());
 				if(partsys->mPartSysData.mBurstRadius != 0)
