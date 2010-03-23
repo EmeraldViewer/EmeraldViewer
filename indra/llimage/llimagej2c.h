3ee1ebcbbf67ccf073c223d924f08a827eb31410
Phox <none@none>
2010-03-23 02:26:35 +0000

    Emkdu support
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401710

--- indra/llimage/llimagej2c.h
+++ indra/llimage/llimagej2c.h
@@ -37,6 +37,7 @@
 #include "llassettype.h"
 
 class LLImageJ2CImpl;
+class EMKDUImpl;
 class LLImageJ2C : public LLImageFormatted
 {
 protected:
@@ -78,6 +79,7 @@ public:
 	static void openDSO();
 	static void closeDSO();
 	static std::string getEngineInfo();
+	static bool useEMKDU;
 	
 protected:
 	friend class LLImageJ2CImpl;
@@ -91,6 +93,7 @@ protected:
 	F32 mRate;
 	BOOL mReversible;
 	LLImageJ2CImpl *mImpl;
+	EMKDUImpl* emImpl;
 	std::string mLastError;
 };
 
