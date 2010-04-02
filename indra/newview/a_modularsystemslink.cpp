c3a3a2a941fa0f9aa6e96072f8b5c3dea3495137
jcool410 <none@none>
2010-04-02 09:37:55 +0000

    *fix: forgot two files
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401750

---
+++ indra/newview/a_modularsystemslink.cpp
@@ -0,0 +1,162 @@
+/* Copyright (c) 2010
+ *
+ * Modular Systems All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *   3. Neither the name Modular Systems nor the names of its contributors
+ *      may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY MODULAR SYSTEMS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL MODULAR SYSTEMS OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "llviewerprecompiledheaders.h"
+
+#include "a_modularsystemslink.h"
+
+#include "llbufferstream.h"
+
+#include "llappviewer.h"
+
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
+
+#include "llsdserialize.h"
+
+#include "llversionviewer.h"
+
+ModularSystemsLink* ModularSystemsLink::sInstance;
+
+ModularSystemsLink::ModularSystemsLink()
+{
+	sInstance = this;
+}
+
+ModularSystemsLink::~ModularSystemsLink()
+{
+	sInstance = NULL;
+}
+
+ModularSystemsLink* ModularSystemsLink::getInstance()
+{
+	if(sInstance)return sInstance;
+	else
+	{
+		sInstance = new ModularSystemsLink();
+		return sInstance;
+	}
+}
+
+static const std::string versionid = llformat(" %s %d.%d.%d (%d)", LL_CHANNEL, LL_VERSION_MAJOR, LL_VERSION_MINOR, LL_VERSION_PATCH, LL_VERSION_BUILD);
+
+void ModularSystemsLink::start_download()
+{
+	std::string url = "http://modularsystems.sl/app/msdata/";
+	LLSD headers;
+	headers.insert("Accept", "*/*");
+	headers.insert("User-Agent", LLAppViewer::instance()->getWindowTitle());
+	headers.insert("viewer-version", versionid);
+
+	LLHTTPClient::get(url,new ModularSystemsDownloader( ModularSystemsLink::msdata ),headers);
+}
+void cmdline_printchat(std::string message);
+void ModularSystemsLink::msdata(U32 status, std::string body)
+{
+	ModularSystemsLink* self = getInstance();
+	cmdline_printchat(llformat("%d",status)+"\n"+body);
+
+	LLSD data;
+	std::istringstream istr(body);
+	LLSDSerialize::fromXML(data, istr);
+	if(data.isDefined())
+	{
+		LLSD& support_agents = data["agents"];
+
+		self->personnel.clear();
+		for(LLSD::map_iterator itr = support_agents.beginMap(); itr != support_agents.endMap(); ++itr)
+		{
+			std::string key = (*itr).first;
+			LLSD& content = (*itr).second;
+			U8 val;
+			if(content.has("support"))val = val | EM_SUPPORT;
+			if(content.has("developer"))val = val | EM_DEVELOPER;
+			self->personnel[LLUUID(key)] = val;
+		}
+
+		LLSD& blocked = data["blocked"];
+
+		self->blocked_versions.clear();
+		for (LLSD::array_iterator itr = blocked.beginArray(); itr != blocked.endArray(); ++itr)
+		{
+			std::string vers = (*itr).asString();
+			self->blocked_versions.insert(vers);
+		}
+	}
+
+	//LLSD& dev_agents = data["dev_agents"];
+	//LLSD& client_ids = data["client_ids"];
+}
+
+BOOL ModularSystemsLink::is_support(LLUUID id)
+{
+	ModularSystemsLink* self = getInstance();
+	if(self->personnel.find(id) != self->personnel.end())
+	{
+		return ((self->personnel[id] & EM_SUPPORT) != 0) ? TRUE : FALSE;
+	}
+	return FALSE;
+}
+
+BOOL ModularSystemsLink::is_developer(LLUUID id)
+{
+	ModularSystemsLink* self = getInstance();
+	if(self->personnel.find(id) != self->personnel.end())
+	{
+		return ((self->personnel[id] & EM_DEVELOPER) != 0) ? TRUE : FALSE;
+	}
+	return FALSE;
+}
+
+BOOL ModularSystemsLink::allowed_login()
+{
+	ModularSystemsLink* self = getInstance();
+	return (self->blocked_versions.find(versionid) == self->blocked_versions.end());
+}
+
+
+ModularSystemsDownloader::ModularSystemsDownloader(void (*callback)(U32, std::string)) : mCallback(callback) {}
+ModularSystemsDownloader::~ModularSystemsDownloader(){}
+void ModularSystemsDownloader::error(U32 status, const std::string& reason){}
+void ModularSystemsDownloader::completedRaw(
+			U32 status,
+			const std::string& reason,
+			const LLChannelDescriptors& channels,
+			const LLIOPipe::buffer_ptr_t& buffer)
+{
+	LLBufferStream istr(channels, buffer.get());
+	std::stringstream strstrm;
+	strstrm << istr.rdbuf();
+	std::string result = std::string(strstrm.str());
+	mCallback(status, result);
+}
+
+
