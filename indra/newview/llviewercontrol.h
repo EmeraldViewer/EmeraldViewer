638752f9cbad2e7a92d298a4e2ee9d670549bb38
Draconis <none@none>
2010-04-06 15:17:44 +0000

    was talking to j in skype and i think we came up with a fix? it compiles for me, basically just made declarations in the header, and usages in the cpp as per what should be done usually?
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401771

--- indra/newview/llviewercontrol.h
+++ indra/newview/llviewercontrol.h
@@ -246,10 +246,10 @@ public:
 	}
 };
 
-template <>					void jc_rebind::rebind_callback<S32>(const LLSD &data, S32 *reciever){ *reciever = data.asInteger(); }
-template <>					void jc_rebind::rebind_callback<F32>(const LLSD &data, F32 *reciever){ *reciever = data.asReal(); }
-template <>					void jc_rebind::rebind_callback<U32>(const LLSD &data, U32 *reciever){ *reciever = data.asInteger(); }
-template <>					void jc_rebind::rebind_callback<std::string>(const LLSD &data, std::string *reciever){ *reciever = data.asString(); }
+template <>					void jc_rebind::rebind_callback<S32>(const LLSD &data, S32 *reciever);
+template <>					void jc_rebind::rebind_callback<F32>(const LLSD &data, F32 *reciever);
+template <>					void jc_rebind::rebind_callback<U32>(const LLSD &data, U32 *reciever);
+template <>					void jc_rebind::rebind_callback<std::string>(const LLSD &data, std::string *reciever);
 
 }
 using namespace jc_you_suck;
