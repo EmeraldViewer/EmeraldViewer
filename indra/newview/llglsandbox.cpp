f2d5ffc9fae14de58564e5973c503c9a0647e6f6
jcool410 <none@none>
2010-04-07 11:45:38 +0000

    http://secondlife.com/corporate/tpv.php section 1. b. references 9. ad. links to http://wiki.secondlife.com/wiki/Channel_and_Version_Requirements
    which state that
    1. Choose a Version Name
    The default Version Name is "Second Life"
    2. Choose a Version Variant. The default variant is "Release"
    The combination of the Name and Variant should be combined to make the Channel Name.
    The default Channel Name is "Second Life Release"
    
    Therefore "Emerald Viewer" as a channel is not TPVP compliant :| *fixes*
    
    also some gsaved -> rebinds optimizations
    
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401778

--- indra/newview/llglsandbox.cpp
+++ indra/newview/llglsandbox.cpp
@@ -71,7 +71,9 @@
  
 BOOL LLAgent::setLookAt(ELookAtType target_type, LLViewerObject *object, LLVector3 position)
 {
-	if(!gSavedSettings.getBOOL("EmeraldBroadcastPointers"))
+	static BOOL* sEmeraldBroadcastPointers = rebind_llcontrol<BOOL>("EmeraldBroadcastPointers", &gSavedSettings, true);
+
+	if(!(*sEmeraldBroadcastPointers))
 	{
 		if(!mLookAt || mLookAt->isDead())
 			return FALSE;
@@ -103,8 +105,10 @@ BOOL LLAgent::setLookAt(ELookAtType target_type, LLViewerObject *object, LLVecto
 
 BOOL LLAgent::setPointAt(EPointAtType target_type, LLViewerObject *object, LLVector3 position)
 {
+	static BOOL* sEmeraldBroadcastPointers2 = rebind_llcontrol<BOOL>("EmeraldBroadcastPointers2", &gSavedSettings, true);
+
 	// disallow pointing at attachments and avatars
-	if ((object && (object->isAttachment() || object->isAvatar())) || !gSavedSettings.getBOOL("EmeraldBroadcastPointers2"))
+	if ((object && (object->isAttachment() || object->isAvatar())) || !(*sEmeraldBroadcastPointers2))
 	{
 		return FALSE;
 	}
