f192ef9752539b2655ea152b4b8140b8fa059c48
JessicaLyon <none@none>
2010-04-30 20:29:02 +0000

    Changed channel name from release to Internal.
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401831

--- indra/llcommon/llversionviewer.h
+++ indra/llcommon/llversionviewer.h
@@ -38,7 +38,7 @@ const S32 LL_VERSION_MINOR = 3;
 const S32 LL_VERSION_PATCH = 2;
 const S32 LL_VERSION_BUILD = 75;
 
-const char * const LL_CHANNEL = "Emerald Viewer Release";
+const char * const LL_CHANNEL = "Emerald Viewer Internal";
 
 #if LL_DARWIN
 const char * const LL_VERSION_BUNDLE_ID = "com.secondlife.emerald.viewer";
