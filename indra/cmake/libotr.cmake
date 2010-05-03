f2582882c71b41fb63e5a0ae15bb73a5143e5e5b
discrete <none@none>
2010-05-03 18:31:08 +0000

    Added packages for OTR in Darwin and Linux
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401854

--- indra/cmake/libotr.cmake
+++ indra/cmake/libotr.cmake
@@ -1,8 +1,15 @@
 # -*- cmake -*-
+include(Prebuilt)
+
+use_prebuilt_binary(otr)
+
+set(OTR_LIBRARY otr)
+if (NOT LINUX)
+	set(GCRYPT_LIBRARY gcrypt.11)
+	set(GPG-ERROR_LIBRARY gpg-error.0)
+endif (NOT LINUX)
 
 set(LIBOTR_INCLUDE_DIRS
     )
 
-set(LIBOTR_LIBRARIES
-    libotr
-    )
+set(LIBOTR_LIBRARIES libotr)
