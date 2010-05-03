4d3e793197d60a672cd7f2f54558a843641b2ffc
hyang <none@none>
2010-05-03 17:20:58 +0000

    --HG-- extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401849

--- indra/newview/viewer_manifest.py
+++ indra/newview/viewer_manifest.py
@@ -56,6 +56,12 @@ class ViewerManifest(LLManifest):
 
             # include the entire shaders directory recursively
             self.path("shaders")
+            # include the entire beams directory
+            self.path("beams")
+            self.path("beamsColors")
+	    self.path("bridge_assets")
+	    self.path("dictionaries")
+
             # ... and the entire windlight directory
             self.path("windlight")
             self.end_prefix("app_settings")
