84f511ed471a332846f9e8210f928142f87adc25
lordgreggreg <none@none>
2010-05-04 18:39:19 +0000

    * more windows cmake fixes
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401860

--- indra/cmake/libotr.cmake
+++ indra/cmake/libotr.cmake
@@ -4,15 +4,16 @@ include(Prebuilt)
 use_prebuilt_binary(otr)
 
 #set(OTR_LIBRARY otr)
+set(LIBOTR_LIBRARY
+    otr
+    )
 if (NOT LINUX)
 	set(GCRYPT_LIBRARY libgcrypt)
 	set(GPG-ERROR_LIBRARY libgpg-error)
-	set(OTR_LIBRARY libotr)
+	set(LIBOTR_LIBRARY libotr)
 endif (NOT LINUX)
 
 set(LIBOTR_INCLUDE_DIRS
     )
 
-set(LIBOTR_LIBRARY
-    otr
-    )
+
