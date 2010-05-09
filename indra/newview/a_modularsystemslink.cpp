d2eea4d657ec55cc8531fac82ce8e74cfe273b5e
lordgreggreg <none@none>
2010-05-09 08:10:21 +0000

    * Emerald should NEVER under any circumstances FORCE a user to give out information * Especially without them even knowing * Especially when they aren't even looking for support * I do not personally know everyone in support, nor do I want them to have information about what I am using without my knowledge or consent.
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401896

--- indra/newview/a_modularsystemslink.cpp
+++ indra/newview/a_modularsystemslink.cpp
@@ -175,7 +175,7 @@ std::string ModularSystemsLink::processRequestForInfo(LLUUID requester, std::str
 	std::string my_name;
 	gAgent.buildFullname(my_name);
 	// If it's an unsupported build, say that.
-	if(LL_CHANNEL != EMERALD_RELEASE_CHANNEL)
+	/*if(LL_CHANNEL != EMERALD_RELEASE_CHANNEL)
 	{
 		pack_instant_message(
 			 gMessageSystem,
@@ -190,7 +190,7 @@ std::string ModularSystemsLink::processRequestForInfo(LLUUID requester, std::str
 			 sessionid
 		);
 		gAgent.sendReliableMessage();
-	}
+	}*/
 	
 	//llinfos << "sysinfo was found in this message, it was at " << message.find("/sysinfo") << " pos." << llendl;
 	std::string outmessage("I am requesting information about your system setup.");
