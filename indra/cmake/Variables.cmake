8b7d66f940dc32ede6aa39c83ffdd848e6c5cc2d
discrete <none@none>
2010-05-18 15:56:56 +0000

    Changed CMake variable VIEWER_CHANNEL to "Emerald Viewer". Settings file --settings options become "settings_emeraldviewer.xml" --channel also becomes "Emerald Viewer" which may or may not even matter, and can be killed off..
    --HG--
    extra : convert_revision : svn%3A1bd1a86a-1eb0-4ff1-abcc-96d0af321e0c/trunk%401964

--- indra/cmake/Variables.cmake
+++ indra/cmake/Variables.cmake
@@ -96,7 +96,7 @@ endif (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 set(GRID agni CACHE STRING "Target Grid")
 
 set(VIEWER ON CACHE BOOL "Build Second Life viewer.")
-set(VIEWER_CHANNEL "Emerald Viewer Internal" CACHE STRING "Viewer Channel Name")
+set(VIEWER_CHANNEL "Emerald Viewer" CACHE STRING "Viewer Channel Name")
 set(VIEWER_LOGIN_CHANNEL ${VIEWER_CHANNEL} CACHE STRING "Fake login channel for A/B Testing")
 set(VIEWER_BRANDING_ID "snowglobe" CACHE STRING "Viewer branding id (currently secondlife|snowglobe)")
 
